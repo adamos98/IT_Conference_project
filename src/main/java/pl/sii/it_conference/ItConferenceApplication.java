d3f2cea943bb62d130f3aed4afdf06f155795990
adamos98 <Zal.pl13579*>
2021-05-31 16:38:28 +0200

    Initial commit

---
+++ src/main/java/pl/sii/it_conference/ItConferenceApplication.java
@@ -0,0 +1,13 @@
+package pl.sii.it_conference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItConferenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItConferenceApplication.class, args);
+    }
+
+}
