f4ba6897f763ed936a9d9ccf92f27abe78814c55
adamos98 <Zal.pl13579*>
2021-06-03 21:03:38 +0200

    Exception handling and displaying commit

--- src/main/java/pl/sii/it_conference/exceptions/CustomExceptionHandler.java
+++ src/main/java/pl/sii/it_conference/exceptions/CustomExceptionHandler.java
@@ -1,68 +1,93 @@
 package pl.sii.it_conference.exceptions;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
+
 @ControllerAdvice
 public class CustomExceptionHandler {
 
     @ExceptionHandler(value = {NotFoundException.class})
-    public ResponseEntity<Object> handleNotFoundException(NotFoundException e){
+    public String handleNotFoundException(NotFoundException e, Model model){
         ApiException apiException = new ApiException(
                 e.getMessage(),
                 HttpStatus.NOT_FOUND,
                 ZonedDateTime.now(ZoneId.of("Z")));
-        return new ResponseEntity<>(apiException, HttpStatus.NOT_FOUND);
+        model.addAttribute("message",e.getMessage());
+        model.addAttribute("httpStatus",HttpStatus.NOT_FOUND);
+        model.addAttribute("zonedDateTime",ZonedDateTime.now(ZoneId.of("Z")));
+        //return new ResponseEntity<>(apiException, HttpStatus.NOT_FOUND);
+        return "error";
     }
 
     @ExceptionHandler(value = {NotDeletedException.class})
-    public ResponseEntity<Object> handleNotDeletedException(NotDeletedException e){
+    public String handleNotDeletedException(NotDeletedException e, Model model){
         ApiException apiException = new ApiException(
                 e.getMessage(),
                 HttpStatus.BAD_REQUEST,
                 ZonedDateTime.now(ZoneId.of("Z")));
-        return new ResponseEntity<>(apiException, HttpStatus.BAD_REQUEST);
+        model.addAttribute("message",e.getMessage());
+        model.addAttribute("httpStatus",HttpStatus.BAD_REQUEST);
+        model.addAttribute("zonedDateTime",ZonedDateTime.now(ZoneId.of("Z")));
+        //return new ResponseEntity<>(apiException, HttpStatus.BAD_REQUEST);
+        return "error";
     }
 
     @ExceptionHandler(value = {FullPrelectionException.class})
-    public ResponseEntity<Object> handleFullPrelectionException(FullPrelectionException e){
+    public String handleFullPrelectionException(FullPrelectionException e, Model model){
         ApiException apiException = new ApiException(
                 e.getMessage(),
                 HttpStatus.BAD_REQUEST,
                 ZonedDateTime.now(ZoneId.of("Z")));
-        return new ResponseEntity<>(apiException, HttpStatus.BAD_REQUEST);
+        model.addAttribute("message",e.getMessage());
+        model.addAttribute("httpStatus",HttpStatus.BAD_REQUEST);
+        model.addAttribute("zonedDateTime",ZonedDateTime.now(ZoneId.of("Z")));
+        //return new ResponseEntity<>(apiException, HttpStatus.BAD_REQUEST);
+        return "error";
     }
 
     @ExceptionHandler(value = {LoginAndEmailRegisteredException.class})
-    public ResponseEntity<Object> handleLoginAndEmailRegisteredException(LoginAndEmailRegisteredException e){
+    public String handleLoginAndEmailRegisteredException(LoginAndEmailRegisteredException e, Model model){
         ApiException apiException = new ApiException(
                 e.getMessage(),
                 HttpStatus.BAD_REQUEST,
                 ZonedDateTime.now(ZoneId.of("Z")));
-        return new ResponseEntity<>(apiException, HttpStatus.BAD_REQUEST);
+        model.addAttribute("message",e.getMessage());
+        model.addAttribute("httpStatus",HttpStatus.BAD_REQUEST);
+        model.addAttribute("zonedDateTime",ZonedDateTime.now(ZoneId.of("Z")));
+        //return new ResponseEntity<>(apiException, HttpStatus.BAD_REQUEST);
+        return "error";
     }
 
     @ExceptionHandler(value = {LoginRegisteredException.class})
-    public ResponseEntity<Object> handleLoginRegisteredException(LoginRegisteredException e){
+    public String handleLoginRegisteredException(LoginRegisteredException e, Model model){
         ApiException apiException = new ApiException(
                 e.getMessage(),
                 HttpStatus.BAD_REQUEST,
                 ZonedDateTime.now(ZoneId.of("Z")));
-        return new ResponseEntity<>(apiException, HttpStatus.BAD_REQUEST);
+        model.addAttribute("message",e.getMessage());
+        model.addAttribute("httpStatus",HttpStatus.BAD_REQUEST);
+        model.addAttribute("zonedDateTime",ZonedDateTime.now(ZoneId.of("Z")));
+        //return new ResponseEntity<>(apiException, HttpStatus.BAD_REQUEST);
+        return "error";
     }
 
     @ExceptionHandler(value = {UserAlreadyReservedPrelectionOnThatTimeException.class})
-    public ResponseEntity<Object> handleUserAlreadyReservedPrelectionOnThatTimeException(UserAlreadyReservedPrelectionOnThatTimeException e){
+    public String handleUserAlreadyReservedPrelectionOnThatTimeException(UserAlreadyReservedPrelectionOnThatTimeException e, Model model){
         ApiException apiException = new ApiException(
                 e.getMessage(),
                 HttpStatus.BAD_REQUEST,
                 ZonedDateTime.now(ZoneId.of("Z")));
-        return new ResponseEntity<>(apiException, HttpStatus.BAD_REQUEST);
+        model.addAttribute("message",e.getMessage());
+        model.addAttribute("httpStatus",HttpStatus.BAD_REQUEST);
+        model.addAttribute("zonedDateTime",ZonedDateTime.now(ZoneId.of("Z")));
+        //return new ResponseEntity<>(apiException, HttpStatus.BAD_REQUEST); //return jako API
+        return "error"; //Thymeleaf widok
     }
 
 }
