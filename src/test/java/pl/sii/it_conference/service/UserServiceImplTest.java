c4c47b790cace27dc6db5cce0281f5fe66a3de8d
adamos98 <Zal.pl13579*>
2021-06-03 23:16:45 +0200

    UserServiceImplTest commit

---
+++ src/test/java/pl/sii/it_conference/service/UserServiceImplTest.java
@@ -0,0 +1,103 @@
+package pl.sii.it_conference.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
+import org.modelmapper.ModelMapper;
+import pl.sii.it_conference.ModelUtils;
+import pl.sii.it_conference.TestConst;
+import pl.sii.it_conference.dto.UserDto;
+import pl.sii.it_conference.dto.UserWithIdDto;
+import pl.sii.it_conference.entity.User;
+import pl.sii.it_conference.repository.UserRepository;
+import pl.sii.it_conference.serviceImplementation.UserServiceImpl;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+class UserServiceImplTest {
+
+    @Mock
+    UserRepository userRepository;
+
+    @InjectMocks
+    private UserServiceImpl userService;
+
+    @Mock
+    private ModelMapper modelMapper;
+
+    private UserWithIdDto userWithIdDto = UserWithIdDto.builder()
+            .id(1L)
+            .login("Tester2")
+            .email("tester2@gmail.com")
+            .build();
+
+    private User user = User.builder()
+            .id(1L)
+            .login("Tester2")
+            .email("tester2@gmail.com")
+            .build();
+    private User user2 = User.builder()
+            .id(1L)
+            .login("Testing")
+            .email("Testing@testing.com")
+            .build();
+
+    private UserDto userDto = UserDto.builder()
+            .email(TestConst.EMAIL)
+            .login(TestConst.LOGIN)
+            .build();
+
+    private String userEmail = user.getEmail();
+
+    @Test
+    void getUserById() {
+        Long id = 1L;
+        User user = new User();
+        user.setId(1L);
+
+        when(userRepository.findById(id)).thenReturn(Optional.of(user));
+        when(modelMapper.map(user, UserWithIdDto.class)).thenReturn(userWithIdDto);
+        assertEquals(userWithIdDto,userService.getUserById(id));
+        verify(userRepository,times(1)).findById(id);
+    }
+
+    @Test
+    void findByLogin() {
+        User user = new User();
+        user.setLogin(TestConst.LOGIN);
+        when(userRepository.findByLogin(TestConst.LOGIN)).thenReturn(Optional.of(user));
+        when(modelMapper.map(user,UserWithIdDto.class)).thenReturn(userWithIdDto);
+        assertEquals(userWithIdDto,userService.findByLogin(TestConst.LOGIN));
+        verify(userRepository,times(1)).findByLogin(TestConst.LOGIN);
+    }
+
+    @Test
+    void save() {
+        when(userRepository.findByLogin(userEmail)).thenReturn(Optional.ofNullable(user));
+        when(userService.findByLogin(userEmail)).thenReturn(userWithIdDto);
+        when(modelMapper.map(userWithIdDto,User.class)).thenReturn(user);
+        when(userRepository.save(user)).thenReturn(user);
+        when(modelMapper.map(user,UserDto.class)).thenReturn(userDto);
+    }
+
+    @Test
+    void updateEmail() {
+        when(userRepository.findByLogin(anyString())).thenReturn(Optional.of(user));
+        when(userRepository.save(any())).thenReturn(user);
+    }
+
+    @Test
+    void getAllUsers() {
+        when(userRepository.findAll()).thenReturn(List.of(user));
+    }
+}
